7c4c5c57fb55fa32573efc5934c014cc0a35b0bb
Fabio Garcia <fabio@blastpress.com>
2014-04-09 19:07:30 -0400

    Litecage JS

--- webapp/layouts/layout.php
+++ webapp/layouts/layout.php
@@ -3,6 +3,7 @@
 <?php if($js) echo $js."\n"; ?>
 <?php if($css) echo $css."\n"; ?>
 <title><?php echo $title; ?></title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
 <script type='text/javascript' src='/js/litecage.js'></script>
 </head>
 <body >
