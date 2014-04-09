f77451e8fb76d8aa49a759609fd3e81379142eed
Fabio Garcia <fabio@blastpress.com>
2014-04-09 19:05:54 -0400

    Litecage JS

---
+++ webapp/layouts/layout.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<?php if($js) echo $js."\n"; ?>
+<?php if($css) echo $css."\n"; ?>
+<title><?php echo $title; ?></title>
+<script type='text/javascript' src='/js/litecage.js'></script>
+</head>
+<body >
+	<?php echo $content."\n"; ?>
+</body>
+</html>
